d4c80d3a6b429fd777d280fdc5f5f0d7e373aea4
Zeeshan <zeeshan.emp@gmail.com>
2016-12-03 09:09:10 -0500

    Unit Class Added, Address Class Added

---
+++ GitHome/Models/Unit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace GitHome.Models
+{
+    public class Unit
+    {
+        public int unitID { get; set; }
+
+        public int agentID { get; set; }
+        public int clientID { get; set; }
+        public Address unitAddress { get; set; }
+        public int yearBuilt { get; set; }
+        public int numRooms { get; set; }
+        public int numBathrooms { get; set; }
+        public int numBedrooms { get; set; }
+        public int numGarageDoors { get; set; }
+        public bool centralAirConditioning { get; set; }
+        public string description { get; set; }
+        public List<Image> unitPicture { get; set; }
+            
+        public int askingPrice { get; set; }
+    }
+}
